7923e3ef3b4dffeedcd2c0f68d1aeea9d16b9fa3
wilmansirit <wsirit@gmail.com>
2020-06-08 08:23:07 -0400

    Creating the server with express

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 
 import 'reflect-metadata';
 import { DatabaseConnection } from './database/databaseConnection';
-import { User } from "./entity/User"
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Request, Response } from "express";
+import { Routes } from "./routes";
 
 DatabaseConnection.configure({
     type: 'mysql',
@@ -18,8 +21,33 @@ console.log("Numero de Instancias creadas: ", DatabaseConnection.numeroInstancia
 
 createConnection.then(async connection => {
 
-    const users = await connection.manager.find(User)
-    console.log(users);
+    // create express app
+    const app = express();
+    app.use(bodyParser.json());
+
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+
+            const result = (new (route.controller as any))[route.action](req, res, next);
+
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+
+            } else if (result !== null && result !== undefined) {
+                res.json(result);
+            }
+
+        });
+    });
+
+    // setup express app here
+    // ...
+
+    // start express server
+    app.listen(3000);
+    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
 
 }).catch(err => console.log(err));
 
